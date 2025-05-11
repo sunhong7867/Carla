b5ccb776fcaa3cc54e1a9c7f7619cbeaaeb92a7a
sunhong7867 <tjsghd7867@g.skku.edu>
2025-05-11 16:11:33 +0900

    ACC_S001 시나리오 SpeedAction 값 설정 변경

--- dev/Control/autonomous_driving_system.py
+++ dev/Control/autonomous_driving_system.py
@@ -163,20 +163,18 @@ class AutonomousDrivingSystem(BasicControl):
 
         # === Debugging Print ===
         print("======================================")
-        #print(f"[EGO] Velocity: {ego_data.velocity_x:.2f} m/s | Accel: {ego_data.accel_x:.2f} m/s² | Heading: {ego_data.heading:.2f}°")
-        #print(f"[ACC] Mode: {acc_mode.name} | ACC Accel: {acc_accel:.2f} m/s²")
+        print(f"[EGO] Velocity: {ego_data.velocity_x:.2f} m/s | Accel: {ego_data.accel_x:.2f} m/s² | Heading: {ego_data.heading:.2f}°")
+        print(f"[ACC] Mode: {acc_mode.name} | ACC Accel: {acc_accel:.2f} m/s²")
         # print(f"[AEB] Mode: {aeb_mode.name} | AEB Accel: {aeb_accel:.2f} m/s²")
         # print(f"[LFA] Mode: {lfa_mode.name} | Steer_PID: {steer_pid:.2f}° | Steer_Stanley: {steer_sta:.2f}° | Final Steer: {steer_cmd:.2f}°")
         #print(f"[CTRL] Throttle: {self.control.throttle:.2f} | Brake: {self.control.brake:.2f} | Steer: {self.control.steer:.2f}")
-        print(f"[Filtered] {len(filtered)} objects")
-        if filtered:
-            for i, obj in enumerate(filtered):
-                print(
-                    f"  F{i}: ID={obj.object_id}, dist={obj.distance:.2f}, status={obj.status.name}, cell={obj.cell_id}")
-
-        print(f"[Predicted] {len(predicted)} objects")
-        print(
-            f"[ACC Target] ID={acc_target.object_id}, Dist={acc_target.distance:.2f}, Mode={acc_mode.name}, Accel={acc_accel:.2f}")
+        #print(f"[Filtered] {len(filtered)} objects")
+        #if filtered:
+        #    for i, obj in enumerate(filtered):
+        #        print(
+        #            f"  F{i}: ID={obj.object_id}, dist={obj.distance:.2f}, status={obj.status.name}, cell={obj.cell_id}")
+        #print(f"[Predicted] {len(predicted)} objects")
+        #print(f"[ACC Target] ID={acc_target.object_id}, Dist={acc_target.distance:.2f}, Mode={acc_mode.name}, Accel={acc_accel:.2f}")
 
     def reset(self):
         if hasattr(self, "_imu_sensor"):
